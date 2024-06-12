c4a592b7e954fead3b48886c98906df1f46f81a5
Matt Doyle <doyle9214@gmail.com>
2024-06-12 14:28:25 +0100

    updated security

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,6 +2,7 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
+import { motion } from "framer-motion";
 
 const TAB_DATA = [
   {
@@ -50,49 +51,55 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="" alt="Luffy placeholder" width={500} height={500} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            Passionate Junior Software Developer with a diverse background
-            spanning education, tech retail, and recruitment. Proficient in
-            test-driven development, object-oriented programming, and full-stack
-            development, I bring a unique blend of technical expertise and
-            customer-centric skills honed from previous careers. Eager to
-            leverage my problem-solving abilities and communication prowess to
-            develop impactful solutions that meet real-world needs.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab)?.content}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <section className="text-white" id="about">
+        <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+          <Image src="" alt="Luffy placeholder" width={500} height={500} />
+          <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+            <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+            <p className="text-base lg:text-lg">
+              Passionate Junior Software Developer with a diverse background
+              spanning education, tech retail, and recruitment. Proficient in
+              test-driven development, object-oriented programming, and
+              full-stack development, I bring a unique blend of technical
+              expertise and customer-centric skills honed from previous careers.
+              Eager to leverage my problem-solving abilities and communication
+              prowess to develop impactful solutions that meet real-world needs.
+            </p>
+            <div className="flex flex-row justify-start mt-8">
+              <TabButton
+                selectTab={() => handleTabChange("skills")}
+                active={tab === "skills"}
+              >
+                {" "}
+                Skills{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("education")}
+                active={tab === "education"}
+              >
+                {" "}
+                Education{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("certifications")}
+                active={tab === "certifications"}
+              >
+                {" "}
+                Certifications{" "}
+              </TabButton>
+            </div>
+            <div className="mt-8">
+              {TAB_DATA.find((t) => t.id === tab)?.content}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </motion.div>
   );
 };
 
